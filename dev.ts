458b3e9b30dcfd2b58d1571bf8de6763e9e4965b
L200416 <167309304+L200416@users.noreply.github.com>
2025-06-20 11:23:35 +0200

    Add files via upload

---
+++ dev.ts
@@ -0,0 +1,12 @@
+
+import { config } from 'dotenv';
+config();
+
+import '@/ai/flows/generate-outfit-inspiration.ts';
+import '@/ai/flows/generate-outfit-suggestion.ts';
+import '@/ai/flows/analyze-clothing-item-flow.ts';
+import '@/ai/flows/generate-outfit-visualization-flow.ts';
+import '@/ai/flows/perform-color-analysis-flow.ts';
+
+
+    
